bbb6208a864ac85812a43e2b79b17ac0a149820d
hungtd <hungtd1@vnpt-technology.vn>
2024-05-23 15:02:35 +0700

    add resource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MultiSelectModule} from "primeng/multiselect";
+import { HeaderComponent } from './component/layout/header/header.component';
+import { FooterComponent } from './component/layout/footer/footer.component';
+import { NavbarComponent } from './component/layout/navbar/navbar.component';
+import { HomepageComponent } from './component/user/homepage/homepage.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {SelectButtonModule} from "primeng/selectbutton";
+import {ScrollTopModule} from "primeng/scrolltop";
+import {ScrollPanelModule} from "primeng/scrollpanel";
+import {TreeSelectModule} from "primeng/treeselect";
+import {ButtonModule} from "primeng/button";
+import {VirtualScrollerModule} from "primeng/virtualscroller";
+import {PaginatorModule} from "primeng/paginator";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MultiSelectModule,
+    ReactiveFormsModule,
+    SelectButtonModule,
+    ScrollPanelModule,
+    ScrollTopModule,
+    TreeSelectModule,
+    ButtonModule,
+    VirtualScrollerModule,
+    PaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/util/util.class.ts
@@ -0,0 +1,59 @@
+import {FormControl, FormGroup} from "@angular/forms";
+
+export class Util {
+  static convertDateToTimeStamp(date: Date): number {
+    return date.getTime();
+  }
+
+  static createFormGroup(formControlModel: any): FormGroup {
+    const group: any = {};
+    Object.keys(formControlModel).forEach(key => {
+      group[formControlModel[key].NAME] = new FormControl({
+        value: formControlModel[key].VALUE || '',
+        disabled: formControlModel[key].DISABLED
+      }, formControlModel[key].VALIDATORS || []);
+    });
+    return new FormGroup(group);
+  }
+
+  static resetFormGroup(form: FormGroup, data?:  { [key: string]: any }, ignore?: string[]) {
+    Object.keys(form.controls).forEach(key => {
+      if (!ignore || !ignore.includes(key)) {
+        const ctr = form.controls[key];
+        if (Array.isArray(ctr.value)) {
+          if (data && data[key]) {
+            ctr.reset(data[key]);
+          } else {
+            ctr.reset([]);
+          }
+        } else {
+          if (data && data[key] != null) {
+            ctr.reset(data[key]);
+          } else {
+            ctr.reset('');
+          }
+        }
+      }
+    });
+  }
+
+  static getDataFormSearch(group: FormGroup): any {
+    const result: { [key: string]: any } = {}; // Specify the type of result object
+    Object.keys(group.controls).forEach(key => {
+      const val = group.controls[key].value;
+      if (!(val == null || val === '' || val.length === 0)) {
+        if (key === 'area') {
+          const areaSelect = group.controls[key].value;
+          result[key] = JSON.stringify(areaSelect.map((ard: any) => ard.id));
+        } else if (val instanceof Date) {
+          result[key] = this.convertDateToTimeStamp(group.controls[key].value);
+        } else if (Array.isArray(val)) {
+          result[key] = JSON.stringify(val);
+        } else {
+          result[key] = val;
+        }
+      }
+    });
+    return result;
+  }
+}
